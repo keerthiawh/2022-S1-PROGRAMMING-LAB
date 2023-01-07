1cd1c95dac5244e033c8ca6a4d052a012844d660
Akash Mohan <90025516+AkashMohan0301@users.noreply.github.com>
2023-01-07 18:51:13 +0530

    Add files via upload

---
+++ S1_07/C02/3.py
@@ -0,0 +1,6 @@
+size = int(input("Enter the size of the list : "))
+list_num = []
+for i in range(size):
+    list_num.append(int(input("Enter the number : ")))
+print("List is ",list_num)
+print("The sum of all items in the list is : ",sum(list_num))
---
+++ S1_07/C02/9.py
@@ -0,0 +1,20 @@
+#create following pattern usinh nested loop
+#
+##
+###
+####
+#####
+####
+###
+##
+#
+
+num = int(input("Enter a number: "))
+for i in range(1, num+1):
+    for j in range(1, i+1):
+        print("*", end=" ")
+    print()
+for i in range(num-1, 0, -1):
+    for j in range(1, i+1):
+        print("*", end=" ")
+    print()
