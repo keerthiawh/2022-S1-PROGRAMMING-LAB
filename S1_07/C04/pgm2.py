646c25aa6cc0529cb7df41747524f3eb95e22a19
Akash Mohan <90025516+AkashMohan0301@users.noreply.github.com>
2023-01-07 13:20:23 +0530

    Add files via upload

---
+++ S1_07/C04/pgm2.py
@@ -0,0 +1,24 @@
+class bank:
+    def __init__(self):
+        self.name=input("Enter your name:")
+        self.accountno=int(input("Enter your account number :"))
+        self.type=input("Enter your account type :")
+        self.current_amount=0
+        
+    def deposit(self):
+        self.d_amount=int(input("enter the amount you are going to deposit"))
+        self.current_amount=self.current_amount+self.d_amount
+    def withdraw(self):
+        self.w_amount=int(input("enter the amountyou are going to withdraw"))
+        self.current_amount=self.current_amount-self.w_amount
+    def display(self):
+        
+        print("name",self.name)
+        print("amount number ",self.accountno)
+        print("account type",self.type)
+        print("balance amount",self.current_amount)
+obj1=bank()
+obj1.deposit()
+obj1.display()
+obj1.withdraw()
+obj1.display()
