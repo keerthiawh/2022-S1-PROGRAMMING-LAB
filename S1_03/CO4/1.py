a1320c2d216ebed07a7d0750fa7c5649a37f2bc3
Ahammed Hani <105771459+AhammedHani@users.noreply.github.com>
2023-01-22 15:53:13 +0530

    Add files via upload

--- S1_03/CO4/1.py
+++ S1_03/CO4/1.py
@@ -1,18 +1,30 @@
-# Create Rectangle class with attributes length and breadth and methods to find area and perimeter.
-class rectangle:
-    def area(self, length, breadth):
-        self.l = length
-        self.b = breadth
-        self.c = length * breadth
-        print("area:", self.c)
+# Create Rectangle class with attributes length and breadth and methods to find area and perimeter. Compare two Rectangle objects by their area.
+class Rectangle:
+    def __init__(self, length, breadth):
+        self.length = length
+        self.breadth = breadth
 
-    def Perimeter(self, length, breadth):
-        c = 2 * (length + breadth)
-        print("perimeter is:", c)
+    def area(self):
+        return self.length * self.breadth
+
+    def perimeter(self):
+        return 2 * (self.length + self.breadth)
 
 
-obj1 = rectangle()
-x = int(input("Enter length and breadth: "))
-y = int(input())
-obj1.area(x, y)
-obj1.Perimeter(x, y)
+l = int(input("Enter object r1 length: "))
+b = int(input("Enter object r1 breadth: "))
+r1 = Rectangle(l, b)
+l = int(input("Enter object r2 length: "))
+b = int(input("Enter object r2 breadth: "))
+r2 = Rectangle(l, b)
+print("Area ")
+print("Area of r1:", r1.area())
+print("Perimeter of r1:", r1.perimeter())
+
+print("Area of r2:", r2.area())
+print("Perimeter of r2:", r2.perimeter())
+
+if r1.area() > r2.area():
+    print("Area of r1 is greater than r2", r1.area())
+else:
+    print("Area of r2 is greater than r1", r2.area())
